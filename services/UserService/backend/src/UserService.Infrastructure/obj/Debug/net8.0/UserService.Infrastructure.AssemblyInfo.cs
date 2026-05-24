8eefab918dd06a18a34dfa5523effd9031b0e0eb
SoumenC-84 <soumen.chakraborty29@gmail.com>
2026-05-24 22:56:59 +0530

    file modified

--- services/UserService/backend/src/UserService.Infrastructure/obj/Debug/net8.0/UserService.Infrastructure.AssemblyInfo.cs
+++ services/UserService/backend/src/UserService.Infrastructure/obj/Debug/net8.0/UserService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a71369f35fb8b98923f1c00f8434d181b1d0f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc33a3892be6e5aea9f7cb53223e7b268f0f33c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
